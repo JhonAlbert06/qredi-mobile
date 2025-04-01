b28ea542ad727e923b64c9bf0152fa76cca18ab4
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2025-04-01 19:44:43 -0400

    refactors

--- app/src/main/java/com/pixelbrew/qredi/admin/Admin_Screen.kt
+++ app/src/main/java/com/pixelbrew/qredi/admin/Admin_Screen.kt
@@ -1,8 +1,6 @@
 package com.pixelbrew.qredi.admin
 
 import android.widget.Toast
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -10,16 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -27,16 +16,13 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.pixelbrew.qredi.MainActivity
-import com.pixelbrew.qredi.R
+import com.pixelbrew.qredi.admin.components.EmailField
+import com.pixelbrew.qredi.admin.components.ForgotPassword
+import com.pixelbrew.qredi.admin.components.HeaderImage
+import com.pixelbrew.qredi.admin.components.LoginButton
+import com.pixelbrew.qredi.admin.components.PasswordField
 import kotlinx.coroutines.launch
 
 @Composable
@@ -104,103 +90,7 @@ fun Login(viewModel: AdminViewModel, modifier: Modifier = Modifier) {
     }
 }
 
-@Composable
-fun LoginButton(modifier: Modifier, isLoginEnabled: Boolean, onLoginSelected: () -> Unit) {
-    Button(
-        onClick = { onLoginSelected() },
-        modifier = modifier
-            .fillMaxWidth()
-            .height(48.dp),
-        shape = MaterialTheme.shapes.large,
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Color(0xFF00BCD4),
-            contentColor = Color.Black,
-            disabledContainerColor = Color(0x2C00BCD4),
-            disabledContentColor = Color(0xFF0C0C0C)
-        ),
-        enabled = isLoginEnabled
 
-    ) {
-        Text(
-            text = "Iniciar sesión",
-            style = MaterialTheme.typography.headlineMedium,
-            color = Color.White
-        )
-    }
-}
 
-@Composable
-fun ForgotPassword(modifier: Modifier) {
-    Text(
-        text = "¿Olvidaste tu contraseña?",
-        style = MaterialTheme.typography.bodyLarge,
-        color = MaterialTheme.colorScheme.primary,
-        modifier = modifier
-            .clickable {}
-            .padding(8.dp),
-        fontWeight = FontWeight.Bold,
-        fontSize = 12.sp
-    )
-}
 
-@Composable
-fun PasswordField(password: String, onValueChange: (String) -> Unit) {
-    TextField(
-        value = password,
-        onValueChange = { onValueChange(it) },
-        modifier = Modifier.fillMaxWidth(),
-        placeholder = {
-            Text(
-                text = "Contraseña",
-                style = MaterialTheme.typography.bodyLarge
-            )
-        },
-        keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Password
-        ),
-        singleLine = true,
-        maxLines = 1,
-        colors = TextFieldDefaults.colors(
-            focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent,
-        ),
 
-        )
-}
-
-@Composable
-fun EmailField(email: String, onValueChange: (String) -> Unit) {
-    TextField(
-        value = email,
-        onValueChange = { onValueChange(it) },
-        modifier = Modifier.fillMaxWidth(),
-        placeholder = {
-            Text(
-                text = "Usuario",
-                style = MaterialTheme.typography.bodyLarge
-            )
-        },
-        keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Email,
-            imeAction = ImeAction.Next
-        ),
-        singleLine = true,
-        maxLines = 1,
-        colors = TextFieldDefaults.colors(
-            focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent,
-        )
-    )
-}
-
-@Composable
-fun HeaderImage(modifier: Modifier) {
-    Image(
-        painter = painterResource(id = R.drawable.icon_app),
-        contentDescription = "Icono de la aplicación",
-        modifier = modifier
-            .size(280.dp)
-            .clip(CircleShape)
-
-    )
-}
---
+++ app/src/main/java/com/pixelbrew/qredi/admin/components/HeaderImage.kt
@@ -0,0 +1,23 @@
+package com.pixelbrew.qredi.admin.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.pixelbrew.qredi.R
+
+@Composable
+fun HeaderImage(modifier: Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.icon_app),
+        contentDescription = "Icono de la aplicación",
+        modifier = modifier
+            .size(280.dp)
+            .clip(CircleShape)
+
+    )
+}
