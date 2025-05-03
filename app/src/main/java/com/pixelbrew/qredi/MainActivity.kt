549a12fdfb5b870f9e800aaa7af4ecd2efb47cdf
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2025-05-03 09:32:25 -0400

    Correcciones Visuales

--- app/src/main/java/com/pixelbrew/qredi/MainActivity.kt
+++ app/src/main/java/com/pixelbrew/qredi/MainActivity.kt
@@ -25,7 +25,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-
         setContent {
             QrediTheme(
                 darkTheme = false,
